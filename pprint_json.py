bcc9721eb43e247f7e07732162f05b60c59b80e6
turim0 <ivan_medvedev_1996@mil.ru>
2019-02-12 01:04:25 +0300

    fixed function and encoding

--- pprint_json.py
+++ pprint_json.py
@@ -4,26 +4,29 @@ import sys
 
 def load_data(file_name):
     try:
-        with open(file_name) as json_file:
+        with open(file_name, encoding='utf-8') as json_file:
             json_string = json_file.read()
-            parsed_dict = json.loads(json_string)
-            return parsed_dict
-    except FileNotFoundError as err:
-        print(err)
+            decoded_json = json.loads(json_string)
+            return decoded_json
+    except FileNotFoundError:
+        return None
     except ValueError:
-        print('No JSON object could be decoded')
+        return None
 
 
-def pretty_print_json(parsed_dict):
-    print(json.dumps(parsed_dict, indent=4))
+def pretty_print_json(decoded_json):
+    print(json.dumps(decoded_json, indent=4))
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1 or sys.argv[1] in {'-h', '--help'}:
         print('Usage: {0} + your_file.json'.format(sys.argv[0]))
         sys.exit()
-    parsed_dict = load_data(sys.argv[1])
-    pretty_print_json(parsed_dict)
+    if load_data(sys.argv[1]) is None:
+        print('Error: file not found or no json object could be decoded')
+        sys.exit()
+    decoded_json = load_data(sys.argv[1])
+    pretty_print_json(decoded_json)
 
 
 
