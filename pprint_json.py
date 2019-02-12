110311b11cfe0a525926b3d33773fd12815e071c
turim0 <ivan_medvedev_1996@mil.ru>
2019-02-12 12:52:18 +0300

    fixed encoding

--- pprint_json.py
+++ pprint_json.py
@@ -8,24 +8,22 @@ def load_data(file_name):
             json_string = json_file.read()
             decoded_json = json.loads(json_string)
             return decoded_json
-    except FileNotFoundError:
+    except (FileNotFoundError, ValueError):
         return None
-    except ValueError:
-        return None
+
 
 
 def pretty_print_json(decoded_json):
-    print(json.dumps(decoded_json, indent=4))
+    print(json.dumps(decoded_json, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1 or sys.argv[1] in {'-h', '--help'}:
         print('Usage: {0} + your_file.json'.format(sys.argv[0]))
         sys.exit()
-    if load_data(sys.argv[1]) is None:
-        print('Error: file not found or no json object could be decoded')
-        sys.exit()
     decoded_json = load_data(sys.argv[1])
+    if decoded_json is None:
+        sys.exit('Error: file not found or no json object could be decoded')
     pretty_print_json(decoded_json)
 
 
